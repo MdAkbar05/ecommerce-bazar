36007ae75b704de2448797e702e7315e6d8a9f33
MdAkbar05 <samratakbar667466@gmail.com>
2025-01-20 23:35:16 +0600

    update cor

--- server/src/app.js
+++ server/src/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173", // Your frontend URL
+    origin: "https://ecommerce-bazar-tau.vercel.app", // Your frontend URL
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   })
 );
