9b5176724c4df1ec3dbf15163d8524c116508316
MdAkbar05 <samratakbar667466@gmail.com>
2025-01-20 23:18:51 +0600

    done

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const app = require("./app");
-const connectDB = require("./config/dbConnect");
+const app = require("./src/app");
+const connectDB = require("./src/config/dbConnect");
 // const logger = require("./controllers/loggerController");
-const { serverPort } = require("./secret");
+const { serverPort } = require("./src/secret");
 app.listen(serverPort, async () => {
   console.log(`Server is running at http://localhost:${serverPort}`);
   // logger.log(`info`, `Server is running at http://localhost:${serverPort}`);
